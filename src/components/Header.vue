40523dfe2a6f2a454f79f0b3134339736104ecbf
Oliver <oliverw@rams.colostate.edu>
2021-12-02 15:16:37 -0700

    13.1 11/16 add page components, routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <router-link class="navbar-brand" to="/">Rockwell Reviews</router-link>
       <button
         class="navbar-toggler"
         type="button"
@@ -14,36 +14,48 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/movies"
+              >Movies</router-link
             >
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/account"
+              >My Account</router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/login"
+              ><button class="btn btn-warning">Login</button></router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              to="/signup"
+              ><button class="btn btn-outline-warning text-dark">
+                Sign Up
+              </button></router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <button class="btn btn-warning">Logout</button>
           </li>
         </ul>
-        <form class="d-flex">
+
+        <!-- <form class="d-flex">
           <input
             class="form-control me-2"
             type="search"
@@ -51,8 +63,14 @@
             aria-label="Search"
           />
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> -->
       </div>
     </div>
   </nav>
 </template>
+
+<style scoped>
+.navbar-nav {
+  align-items: center;
+}
+</style>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+    <h4>Sorry, this page cannot be found</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
